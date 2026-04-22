406f95647d97f5ca465cf8f5437e7d031586640e
sanokata <sanokata@outlook.com>
2026-04-23 06:24:32 +0900

    chore(deps): adapt bevy feature imports and use update_with_threshold

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
+#[cfg(feature = "bevy")]
 use bevy::prelude::*;
+#[cfg(feature = "bevy")]
 use lexaos_bitboard::BitBoard;
+
 pub mod spatial_hash;
-use spatial_hash::SpatialHash;
+pub use spatial_hash::SpatialHash;
 
 /// 空間ハッシュで管理されるエンティティへの付与コンポーネント (汎用版)
+#[cfg(feature = "bevy")]
 #[derive(Component, Debug, Clone)]
 pub struct SpatialManaged {
     /// エンティティの種別インデックス (SpatialHash の E レイヤーに対応)
@@ -14,20 +18,23 @@ pub struct SpatialManaged {
 /// 空間ハッシュの同期と管理を行うプラグイン
 /// const E: エンティティ種別の数
 /// const S: 静的レイヤーの数
+#[cfg(feature = "bevy")]
 pub struct SpatialGridPlugin<const W: usize, const H: usize, const E: usize, const S: usize>;
 
+#[cfg(feature = "bevy")]
 impl<const W: usize, const H: usize, const E: usize, const S: usize> Plugin for SpatialGridPlugin<W, H, E, S> {
     fn build(&self, app: &mut App) {
-        if !app.world().contains_resource::<SpatialHash<W, H, E, S>>() {
-            app.insert_resource(SpatialHash::<W, H, E, S>::default());
+        if !app.world().contains_resource::<SpatialHash<Entity, W, H, E, S>>() {
+            app.insert_resource(SpatialHash::<Entity, W, H, E, S>::default());
         }
         app.add_systems(Update, sync_spatial_hash_system::<W, H, E, S>);
     }
 }
 
 /// Transform の変化を監視し、空間ハッシュの内容を同期 (動的エンティティ)
+#[cfg(feature = "bevy")]
 fn sync_spatial_hash_system<const W: usize, const H: usize, const E: usize, const S: usize>(
-    mut spatial_hash: ResMut<SpatialHash<W, H, E, S>>,
+    mut spatial_hash: ResMut<SpatialHash<Entity, W, H, E, S>>,
     query: Query<(Entity, &Transform, &SpatialManaged), Or<(Changed<Transform>, Added<SpatialManaged>)>>,
     mut removed: RemovedComponents<SpatialManaged>,
 ) {
@@ -41,6 +48,7 @@ fn sync_spatial_hash_system<const W: usize, const H: usize, const E: usize, cons
         let pos = transform.translation;
         let (tile_x, tile_y) = BitBoard::<W, H>::pos_to_tile(pos.x, pos.y);
         
-        spatial_hash.update(entity, (tile_x, tile_y), managed.radius, managed.kind_idx);
+        // スロットリングを活用: 1.0 タイル以上の移動がある場合のみ更新
+        spatial_hash.update_with_threshold(entity, (tile_x, tile_y), managed.radius, managed.kind_idx, 1);
     }
 }
